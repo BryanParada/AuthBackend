d6c0c467709fafb23f1748992a46edfbdaf5d874
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-03 11:10:18 -0300

    V365 - Express Server

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const express = require('express');
+
+//crear el servidor/aplicacion de express
+const app = express();
+const port = 4000;
+
+// GET
+app.get('/', (req, res) =>{
+    // res.status(500).json({
+    res.json({
+        ok: true,
+        msg: 'everything ok',
+        uid: 1234
+    })
+    
+})
+
+// LISTEN
+app.listen( port, () =>{
+   console.log('Server running in port: '+ port );
+   
+} )
+
 console.log('hola desde node!');
+
