21f0dc53ebea0dda3b878ccd8fc231d9eb1bfba1
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-03 16:58:02 -0300

    V377 - Saving user in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,43 +1,73 @@
-const { response } = require('express'); 
+const { response } = require("express");
+const User = require('../models/User');
 
-//Crear nuevo usuario 
-const createUser = (req, res = response) =>{
- 
-    //console.log(req.body);
-    const {email, name, password} = req.body;
-    //console.log(email, name, password);
-    
-    
-    return res.json({
-        ok: true,
-        msg: 'Create User /new'
+//Crear nuevo usuario
+const createUser = async(req, res = response) => {
+  //console.log(req.body);
+  const { email, name, password } = req.body;
+  //console.log(email, name, password);
+
+  try {
+    //Verificar mail
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'This email already exists'
+      })
+    }
+
+    //Crear usuario con el modelo
+    const dbUser = new User(req.body);
+
+
+    // Hash password
+
+    // Generar JWT
+
+    // Crear usuario de DB
+    await dbUser.save();
+
+    // Generar Respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser.id,
+      name, 
     });
-}
 
-//Login usuario 
-const loginUser = (req, res = response) =>{
- 
-    //console.log(req.body);
-     const {email, password} = req.body;
-    // console.log(email, password);
+  } catch (error) {
+    console.log(error);
 
     return res.json({
-        ok: true,
-        msg: 'Login user /'
+      ok: true,
+      msg: "Please contact your Admin",
     });
-}
+  }
+};
+
+//Login usuario
+const loginUser = (req, res = response) => {
+  //console.log(req.body);
+  const { email, password } = req.body;
+  // console.log(email, password);
+
+  return res.json({
+    ok: true,
+    msg: "Login user /",
+  });
+};
 
 // Validar y revalidar token
-const renewToken = (req, res = response) =>{
-    return res.json({
-        ok: true,
-        msg: 'Renew /'
-    });
-}
-
+const renewToken = (req, res = response) => {
+  return res.json({
+    ok: true,
+    msg: "Renew /",
+  });
+};
 
 module.exports = {
-    createUser,
-    loginUser,
-    renewToken
-}
+  createUser,
+  loginUser,
+  renewToken,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -10,23 +10,23 @@ const router = Router();
 
 //Crear nuevo usuario 
 router.post( '/new',
-[
-check('email', 'Email is required').isEmail(),
-check('name', 'Name is required').not().isEmpty(),
-check('password', 'Password must be at least 6 characters').isLength({ min: 6}),
-validateFields //este middleware se ejecutara cuando la ruta sea invocada v
-],
- createUser); //<- mandando como referencia 
+            [
+            check('email', 'Email is required').isEmail(),
+            check('name', 'Name is required').not().isEmpty(),
+            check('password', 'Password must be at least 6 characters').isLength({ min: 6}),
+            validateFields //este middleware se ejecutara cuando la ruta sea invocada v
+            ],
+            createUser); //<- mandando como referencia 
 
 //Login usuario 
 //          ruta,   
 router.post( '/', [
-//middleware
-check('email', 'Email is required').isEmail(),
-check('password', 'Password must be at least 6 characters').isLength({ min: 6}),
-validateFields
+                    //middleware
+                    check('email', 'Email is required').isEmail(),
+                    check('password', 'Password must be at least 6 characters').isLength({ min: 6}),
+                    validateFields
 
-] , loginUser);//controlador ruta
+                    ] , loginUser);//controlador ruta
 
 // Validar y revalidar token
 router.get( '/renew', renewToken);
